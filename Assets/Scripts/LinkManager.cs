7acb59bc1029c2d857d7145dd8a61b4ef92b3717
Kamalc <kimo201224@gmail.com>
2022-05-21 11:26:11 +0200

    Change_Profile

--- Assets/Scripts/LinkManager.cs
+++ Assets/Scripts/LinkManager.cs
@@ -6,27 +6,24 @@ public class LinkManager : MonoBehaviour
 {
     public void openLinkedin()
     {
-        Application.OpenURL("https://www.linkedin.com/in/priyanshu-bhattacharjee/");
+        Application.OpenURL("https://www.linkedin.com/in/mostafa-abdalla-95a971112/");
     }
 
     public void openInstagram()
     {
-        Application.OpenURL("https://www.instagram.com/priyanshucoderx/");
+        Application.OpenURL("https://www.instagram.com/mostafa.abdalla1996/");
     }
 
-    public void openGithub()
-    {
-        Application.OpenURL("https://github.com/Priyanshu-CODERX");
-    }
+   
 
     public void openDiscord()
     {
-        Application.OpenURL("https://discord.gg/5bvZAZd83D");
+        Application.OpenURL("https://discord.gg/7pUZmFxb");
     }
 
     public void openYoutube()
     {
-        Application.OpenURL("https://youtube.com/c/coderslane");
+        Application.OpenURL("https://www.youtube.com/channel/UC6dVpxH2B7K9CSMwN-DuRtw");
     }
 
 }
